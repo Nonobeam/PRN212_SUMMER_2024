67ca5ca5bdd2d4c6a62b6e2ca4fbc7665348d7c0
Nguyen Duc Minh <ducm40877@gmail.com>
2024-07-12 13:18:10 +0700

    no message

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbfd0dce556c91bec77cc47d5492b74268d5db2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5bc9238d85c48a0bc667a3f763cbdf12a02690")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
